a0ca8d1fb9ad121072c3ed44cce1cfdae06692b3
TakanashiHitomi <sf470506412@163.com>
2016-12-01 19:03:05 +0800

    feat:Add runtime permission for app(use RxPermission) feat(build):Add RxJava and RxAndroid

--- app/src/main/java/somi/hitomi/maoyanmovie/activity/MainActivity.java
+++ app/src/main/java/somi/hitomi/maoyanmovie/activity/MainActivity.java
@@ -1,17 +1,21 @@
 package somi.hitomi.maoyanmovie.activity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.SparseArray;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
+import com.tbruyelle.rxpermissions.RxPermissions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.functions.Action1;
 import somi.hitomi.maoyanmovie.R;
 import somi.hitomi.maoyanmovie.common.BaseActivity;
 import somi.hitomi.maoyanmovie.common.BaseFragment;
@@ -35,6 +39,23 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        ).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
+                if (!aBoolean) {
+                    Toast.makeText(MainActivity.this, "为了更好地使用，最好授予本应用权限> <", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "应用已获授权> <", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         initFragment();
         initView();
